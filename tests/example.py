3b90ae23694c16a8404753d2e67720566fc60286
David Kofoed Wind <utdiscant@gmail.com>
2015-07-28 12:02:59 +0200

    Added example of how the API works

--- tests/example.py
+++ tests/example.py
@@ -1,1 +1,11 @@
-__author__ = 'dawi'
+import basset
+
+my_user = basset.Contact()
+my_user.add_name(given_name = "David",
+                 family_name = "Wind",
+                 full_name = "David Kofoed Wind")
+my_user.add_email(address = "utdiscant@gmail.com")
+
+basset.send_interaction(contact=my_user,
+                        transaction_type="signup",
+                        api_key="test")
