71347751c96ab50a5567604d1eb28826c2dfbf57
David Kofoed Wind <utdiscant@gmail.com>
2015-07-28 11:36:40 +0200

    PEP8 compliance and gitignore updates

--- basset/contact_mapper.py
+++ basset/contact_mapper.py
@@ -12,9 +12,9 @@ class ContactMapper():
         self.input_format = input_format()
         self.output_format = output_format()
 
-    def convert(self, input):
+    def convert(self, input_data):
         try:
-            contact_object = self.input_format.to_contact_object(input)
+            contact_object = self.input_format.to_contact_object(input_data)
             return self.output_format.from_contact_object(contact_object)
         except:
             raise ContactConversionException
@@ -29,11 +29,11 @@ class ContactMapperFormat():
 
 
 class ObjectContactMapperFormat(ContactMapperFormat):
-    def to_contact_object(self, input):
-        return input
+    def to_contact_object(self, input_data):
+        return input_data
 
-    def from_contact_object(self, input):
-        return input
+    def from_contact_object(self, input_data):
+        return input_data
 
 
 class GraphContactMapperFormat(ContactMapperFormat):
@@ -53,47 +53,6 @@ class GraphContactMapperFormat(ContactMapperFormat):
     def from_contact_object(self, contact_object):
         raise NotImplementedError
 
-# def prepare_info(info):
-#     info_dict = info.to_dict()
-#     del info_dict['private']
-#     del info_dict['public']
-#     del info_dict['flags']
-#     del info_dict['reference']
-#     if 'label' in info_dict:
-#         del info_dict['label']
-#     if 'data' in info_dict:
-#         del info_dict['data']
-#     return info_dict
-#
-#
-# def to_cypher_dict(dictionary):
-#     return "{" + ", ".join(["%s: '%s'" % (key, value.replace("'","")) for key, value in dictionary.items()]) + "}"
-#
-#
-#
-# class CypherContactMapperFormat():
-#     def to_contact_object(self, cypher):
-#         raise NotImplementedError
-#
-#     def from_contact_object(self, contact_object):
-#         #yield "MERGE (user {email: {E}})", {"E": user.email}
-#
-#         yield "MERGE (contact {id:{I}})", {"I": str(personal["_id"])}
-#         yield "MATCH (u:user {email:{E}}), (p:contact {id:{I}})" + \
-#               "MERGE (u)-[:HAS]->(p)", \
-#               {"I": str(personal["_id"]), "E": user.email}
-#
-#         # make contact info
-#         for info in contact_object:
-#             info_dict = prepare_info(info)
-#             if "%s" % to_cypher_dict(info_dict) == "{}":
-#                 continue
-#             info_cypher = "(i:info:%s %s)" % (info.type, to_cypher_dict(info_dict))
-#             yield "MERGE %s" % info_cypher,
-#             yield "MATCH (p:contact {id:{I}}), %s" % info_cypher + \
-#                   "MERGE (p)-[:CONTAINS]->(i)", \
-#                   {"I": str(personal["_id"])}
-
 
 class JSONContactMapperFormat(ContactMapperFormat):
     def to_contact_object(self, json_contact):
@@ -101,7 +60,7 @@ class JSONContactMapperFormat(ContactMapperFormat):
         return contact_object
 
     def from_contact_object(self, contact_object):
-        json_contact = {}
+        json_contact = dict()
         json_contact['flags'] = contact_object.flags
 
         def recursive_to_dict(something):
